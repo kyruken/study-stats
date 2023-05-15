1eda82756571093f75de58f19eb76ea9468bf187
Avery Kyle Arenas <kyruken@gmail.com>
2023-05-15 15:41:01 -0700

    change timedEvents logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import './App.css'
 import Timer from './components/Timer'
 import { useState } from 'react'
@@ -5,17 +6,33 @@ import { useState } from 'react'
 function App() {
 
   const [isStudy, setIsStudy] = useState(true);
+  const [sessionName, setSessionName] = useState('');
   const [timedEvents, setTimedEvents] = useState([]);
   const time = new Date();
   
   const handleStudyButton = (hours, minutes, seconds) => {
     setIsStudy(prevState => !prevState);
-    timedEvents.push(`${hours}:${minutes}:${seconds}`)
+    setTimedEvents(prevState => {
+      let counter = prevState.length;
+      prevState.push({
+        key: counter+1,
+        number: counter+1,
+        name: sessionName,
+        isStudy: isStudy,
+        hours,
+        minutes,
+        seconds
+      })
+
+      return prevState;
+    })
   }
 
   const timeElements = timedEvents.map(event => {
     return <div>
-      {event}
+      <div>{event.number}. {event.name}</div>
+      {event.isStudy ? <div>Study Session</div> : <div>Break Session</div>}
+      {event.hours}:{event.minutes}:{event.seconds}
     </div>
   })
 
@@ -26,6 +43,7 @@ function App() {
       expiryTimestamp={time}
       handleStudyButton={handleStudyButton}
       ></Timer>
+      <input type='text' placeholder='Session name' onChange={e => setSessionName(e.target.value)}></input>
       {timeElements}
 
     </>
